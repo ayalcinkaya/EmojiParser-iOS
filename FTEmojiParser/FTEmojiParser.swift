1f0a9cf0c3f8c80799042c3c6a0b63ba7a23191d
Florent THOMAS-MOREL <thomasmorel.florent@gmail.com>
2016-04-19 23:16:20 -0400

    Initial Commit

---
+++ FTEmojiParser/FTEmojiParser.swift
@@ -0,0 +1,87 @@
+//
+//  FTEmojiParser.swift
+//  FTEmojiParser
+//
+//  Created by Florent TM on 19/04/2016.
+//  Copyright © 2016 Florent THOMAS-MOREL. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let tables:[String:String] = [
+    ":)" : "😊",
+    ";)" : "😉",
+    ":D" : "😃",
+    ":(" : "😞",
+    ":/" : "🙁",
+    ":$" : "😳",
+    ":P" : "😜",
+    ":*" : "😘",
+    "<3" : "❤️",
+    ":O" : "😮",
+    "(y)" : "👍🏼",
+    "3:)" : "😈",
+    "-_-" : "😑",
+    ":-*" : "😘",
+    ":'(" : "😢",
+    ":-)" : "😊",
+    ";-)" : "😉",
+    "O:)" : "😇",
+    ":poop:" : "💩"
+]
+
+extension UITextView{
+    
+    func postParseStringForEmoji() -> String{
+        let indexes = Array(Set(tables.keys.map({ (key) -> Int in
+            return key.characters.count
+        }))).sort { (a, b) -> Bool in
+            return a > b
+        }
+        
+        var k = 0
+        var res = self.text
+        for i in 0...self.text.characters.count-1{
+            let text = res.substringFromIndex(self.text.startIndex.advancedBy(i))
+           if text.characters.count > 0 {
+                for n in indexes{
+                    if(text.characters.count >= n){
+                        let key = text.substringToIndex(text.startIndex.advancedBy(n))
+                        if let emoji = tables[key]{
+                            let range = res.startIndex.advancedBy(i-k)..<res.startIndex.advancedBy(i+n-k)
+                            res.replaceRange(range, with: emoji)
+                            k+=1
+                        }
+                    }
+                }
+            }
+        }
+        return res
+    }
+    
+    func lastParseStringForEmoji() -> String {
+        let indexes = Array(Set(tables.keys.map({ (key) -> Int in
+            return key.characters.count
+        }))).sort { (a, b) -> Bool in
+            return a > b
+        }
+        
+        if self.text.characters.count > 0 {
+            for n in indexes{
+                if(self.text.characters.count >= n){
+                    let key = self.text.substringFromIndex(self.text.endIndex.advancedBy(-n))
+                    if let emoji = tables[key]{
+                        let range = self.text.endIndex.advancedBy(-n)..<self.text.endIndex
+                        var newText = self.text
+                        newText.removeRange(range)
+                        newText = newText + emoji
+                        return newText
+                    }
+                }
+            }
+        }
+        return self.text
+    }
+    
+}
--- FTEmojiParser/ViewController.swift
+++ FTEmojiParser/ViewController.swift
@@ -8,18 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextViewDelegate {
 
+    @IBOutlet weak var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewDidAppear(animated: Bool) {
+        self.textView.delegate = self
+    }
 
+    func textView(textView: UITextView, shouldChangeTextInRange range: NSRange, replacementText text: String) -> Bool{
+        let customTriggerPonctuationSet = ";.?,! "
+        if customTriggerPonctuationSet.containsString(text){
+            self.textView.text = self.textView.lastParseStringForEmoji()
+        }
+        return true
+    }
 
 }
 
